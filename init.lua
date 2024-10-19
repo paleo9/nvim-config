42dc42de506add34c1426472155d12e8f94f432e
paleo9 <paleo9.dev@gmail.com>
2024-10-19 13:03:13 +0100

    add foldmethod to the modeline

--- init.lua
+++ init.lua
@@ -801,4 +801,4 @@ require('lazy').setup({
 })
 -- }}}
 -- The line beneath this is called `modeline`. See `:help modeline`
--- vim: ts=2 sts=2 sw=2 et
+-- vim: foldmethod=marker ts=2 sts=2 sw=2 expandtab
